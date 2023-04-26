6168ee6882402263322198764608e7879d08e544
Seamas007 <olufy007@gmail.com>
2023-04-26 04:13:35 +0000

    Task api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,48 +1,22 @@
 #!/usr/bin/python3
-"""
-Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress and exports data in CSV format.
-"""
+'''
+A script to export data in the CSV format.
+'''
 
-import requests
 import csv
+import requests
 from sys import argv
 
-
-def get_employee_todos(employee_id):
-    """Returns information about the employee TODO list progress."""
-    user_url = "https://jsonplaceholder.typicode.com/users/" + str(employee_id)
-    todos_url = user_url + "/todos"
-    user_response = requests.get(user_url)
-    todos_response = requests.get(todos_url)
-
-    if user_response.status_code != 200:
-        raise ValueError("User not found")
-    if todos_response.status_code != 200:
-        raise ValueError("Todos not found")
-
-    user_name = user_response.json()["username"]
-    todos = todos_response.json()
-    rows = []
-    for task in todos:
-        row = [employee_id, user_name, task["completed"], task["title"]]
-        rows.append(row)
-    return rows
-
-
-if __name__ == "__main__":
-    if len(argv) != 2 or not argv[1].isdigit():
-        print("Usage: {} employee_id".format(argv[0]))
-        exit(1)
-    try:
-        employee_id = argv[1]
-        rows = get_employee_todos(employee_id)
-        file_name = employee_id + ".csv"
-        with open(file_name, "w", newline="") as csvfile:
-            writer = csv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC)
-            writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-            writer.writerows(rows)
-        print("Data exported to", file_name)
-    except ValueError as e:
-        print(e)
-        exit(1)
+if __name__ == '__main__':
+    uid = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
+    user = requests.get(url, verify=False).json()
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        uid)
+    todo = requests.get(url, verify=False).json()
+    with open("{}.csv".format(uid), 'w', newline='') as csvfile:
+        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for t in todo:
+            taskwriter.writerow([int(uid), user.get('username'),
+                                 t.get('completed'),
+                                 t.get('title')])
